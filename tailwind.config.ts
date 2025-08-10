b6825f380c9bbaae7f923b8c6cbdfbe36e0957fa
Raphael Taylor <ratdanger828@gmail.com>
2025-08-10 13:51:35 +0000

    make the background colour nice i want people to be attracted to the web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
